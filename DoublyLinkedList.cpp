1154759e499aa0e2ba98669a6742660bc8b3cdbf
Lalith-47 <Lalith_kumar_47@outlook.com>
2025-08-05 22:59:24 +0530

    Refactor function declarations and rearrange function definitions in DoublyLinkedList.cpp

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -12,56 +12,14 @@ typedef struct Node{
 node *head = NULL;
 node *tail = NULL;
 node *previous = NULL;
-// Inserting New Node to the Doubly LinkedList
-void append(int data){
-    node *temp = new node{NULL,data,NULL};
-    if(head == NULL){
-        head = tail  = temp;
-    }
-    else 
-    {
-        tail->next = temp;
-        temp->prev= tail;
-        tail = temp;
-    }
-}
 
-//Function to convert doubly LinkedList to Circular doubly LinkedList
-void convertToCircularLinkedList(){
-    head->prev = tail;
-    tail->next = head;
-}
-void traverse(){
-    node *ptr = head;
-    while(ptr!=NULL){
-        cout << "\nPrev: " << ptr->prev << endl;
-        cout << "data: " << ptr->data << endl;
-        cout << "Next: " << ptr->next << endl;
-        ptr = ptr->next;
-    }
-}
+//*All Functions declaration
+void convertToCircularLinkedList(); //Function to convert doubly LinkedList to Circular doubly LinkedList
+void append(int);  // Function to display number of nodes
+void traverse();   //Function to traverse and display all elements
+void display(int); // Function to display number of nodes
+void displayBackward(int); // Function to display and traverse BACKWARDS
 
-//Function to display number of nodes
-void display(int n){
-    node *ptr = head;
-    int i = 0;
-    while(i<n){
-        cout << ptr->data << " -> ";
-        ptr = ptr->next;
-        i++;
-    }
-}
-
-//Function to display and traverse BACKWARDS    
-void displayBackward(int n){
-    node *ptr = tail;
-    int i = 0;
-    while(i<n){
-        cout << ptr->data << " -> ";
-        ptr = ptr->prev;
-        i++;
-    }
-}
 //Body of the main function
 int main()
 {
@@ -73,15 +31,16 @@ int main()
         cin >> data;
         append(data);
     }
-    cout<<"Enter respective number to choose the operation";
-    cout << "\n1: Display all Elements";
-    cout << "\n2: Convert to circular Linked List(Doubly)";
-    cout << "\n3: Display n Elements";
-    cout << "\n4: Display n Elements Backwards\n";
-    cout << "Choice: ";
+    cout<< "Enter respective number to choose the operation:\n"
+    << "1: Display all Elements\n"
+    << "2: Convert to Circular Doubly Linked List\n"
+    << "3: Display n Elements\n"
+    << "4: Display n Elements Backwards\n"
+    << "Choice: ";
+    
     cin >> choice;
     if (choice == 1)
-        traverse();
+    traverse();
     else if(choice == 2){
         convertToCircularLinkedList();
         cout << "Enter no of nodes to display: ";
@@ -91,7 +50,7 @@ int main()
     else if (choice == 3)
     {   cout << "Enter number of Elements: ";
         cin >> x;
-        if(x<n){
+        if(x<=n){
             display(x);
         }
         else{
@@ -104,5 +63,52 @@ int main()
     }
     else{
         cout << "Invalid input\n";
+    }
+}
+void append(int data){
+    node *temp = new node{NULL,data,NULL};
+    if(head == NULL){
+        head = tail  = temp;
+    }
+    else 
+    {
+        tail->next = temp;
+        temp->prev= tail;
+        tail = temp;
+    }
+}
+void traverse()
+{
+    node *ptr = head;
+    while(ptr!=NULL){
+        cout << "\nPrev: " << ptr->prev << endl;
+        cout << "data: " << ptr->data << endl;
+        cout << "Next: " << ptr->next << endl;
+        ptr = ptr->next;
+    }
+}
+// Function to display number of nodes
+void display(int n){
+    node *ptr = head;
+    int i = 0;
+    while(i<n){
+        cout << ptr->data << " -> ";
+        ptr = ptr->next;
+        i++;
+    }
+}
+//Function to convert doubly LinkedList to Circular doubly LinkedList
+void convertToCircularLinkedList(){
+    head->prev = tail;
+    tail->next = head;
+}
+// Function to display and traverse BACKWARDS
+void displayBackward(int n){
+    node *ptr = tail;
+    int i = 0;
+    while(i<n){
+        cout << ptr->data << " -> ";
+        ptr = ptr->prev;
+        i++;
     }
 }
