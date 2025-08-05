09ddf5d65f061026af1b45607588cb5395729d3c
Lalith-47 <Lalith_kumar_47@outlook.com>
2025-08-05 19:42:50 +0530

    Add circular linked list implementation with insertion and display functions

---
+++ CircularLilnkedList.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+//Creation and declaration of Node structure
+typedef struct Node{
+    int data;
+    struct Node *next;
+} node;
+
+//Declaration of head and tail node
+node *head = NULL;
+node *tail = NULL;
+
+// Creation of LinkedList
+void insert(int value){
+    node *temp = new node{value,head};
+    if(head == NULL){
+        head = tail = temp;
+    }
+    else{
+        tail->next = temp;
+        tail = temp;
+        tail->data = value;
+    }
+}
+
+// Display LinkedList
+void display(int stop){
+    int i = 0;
+    node *ptr = head;
+    while(i<stop){
+        cout << ptr->data << " -> ";
+        i++;
+        ptr = ptr->next;
+    }
+}
+
+//Body of the main function
+int main()
+{
+    int n,data;
+    cout << "Enter Number of nodes to insert: ";
+    cin >> n;
+    for (int i = 0; i < n;i++){
+        cout << "Enter Element " << i + 1 << ": ";
+        cin >> data;
+        insert(data);
+    }
+    display(n);
+    return 0;
+}
